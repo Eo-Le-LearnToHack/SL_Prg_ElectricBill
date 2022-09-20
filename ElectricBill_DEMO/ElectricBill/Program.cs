782737b03374f6daf5d375091e8cc893a6b373af
sim8hu <dan.nguyen@rsyd.dk>
2022-09-20 14:50:48 +0200

    add using, namespace, class and main to the file Program.cs

--- ElectricBill_DEMO/ElectricBill/Program.cs
+++ ElectricBill_DEMO/ElectricBill/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ElectricBill
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
